747768219b4bb950418486edcf0adc8c3725c935
aigars.cirulis <aigars.cirulis@accenture.com>
2025-01-31 12:25:19 +0200

    Limit possible field size to range 5 - 40; add comments and summary tags to methods

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
-            
+            // setting the console output encoding to UTF-8 to display emojis correctly
             Console.OutputEncoding = Encoding.UTF8;
 
             Console.WriteLine("Welcome to Conway's Game of Life!");
@@ -14,14 +14,14 @@ namespace GameOfLife
             int size = 0;
             while (true)
             {
-                Console.Write("Please enter the size of the field (e.g., 10 for a 10x10 field): ");
-                if (int.TryParse(Console.ReadLine(), out size) && size > 0)
+                Console.Write("Please enter the size of the field (minimum 5, maximum 40): ");
+                if (int.TryParse(Console.ReadLine(), out size) && size >= 5 && size <= 40)
                 {
                     break;
                 }
                 else
                 {
-                    Console.WriteLine("Invalid input. Please enter a valid positive integer.");
+                    Console.WriteLine("Invalid input. Please enter a valid positive integer between 5 and 40.");
                 }
             }
 
@@ -37,6 +37,11 @@ namespace GameOfLife
             }
         }
 
+        /// <summary>
+        /// Initializes the game field with random live and dead cells.
+        /// </summary>
+        /// <param name="size">The size of the field (NxN), inputted by user.</param>
+        /// <returns>A 2D boolean array representing the game field.</returns>
         static bool[,] InitializeField(int size)
         {
             bool[,] field = new bool[size, size];
@@ -51,6 +56,10 @@ namespace GameOfLife
             return field;
         }
 
+        /// <summary>
+        /// Prints the current state of the game field to the console.
+        /// </summary>
+        /// <param name="field">The game field to print.</param>
         static void PrintField(bool[,] field)
         {
             int size = field.GetLength(0);
@@ -64,6 +73,11 @@ namespace GameOfLife
             }
         }
 
+        /// <summary>
+        /// Updates the game field based on the rules of Conway's Game of Life.
+        /// </summary>
+        /// <param name="field">The current game field.</param>
+        /// <returns>A new game field after applying the rules.</returns>
         static bool[,] UpdateField(bool[,] field)
         {
             int size = field.GetLength(0);
@@ -88,6 +102,13 @@ namespace GameOfLife
             return newField;
         }
 
+        /// <summary>
+        /// Counts the number of live neighbors for a given cell.
+        /// </summary>
+        /// <param name="field">The game field.</param>
+        /// <param name="x">The x-coordinate of the cell.</param>
+        /// <param name="y">The y-coordinate of the cell.</param>
+        /// <returns>The number of live neighbors.</returns>
         static int CountLiveNeighbors(bool[,] field, int x, int y)
         {
             int size = field.GetLength(0);
