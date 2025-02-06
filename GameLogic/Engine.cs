afe2ed81d6d7880982e7039cbd99cfd9fa646d21
aigars.cirulis <aigars.cirulis@accenture.com>
2025-02-06 12:22:50 +0200

    Fix Game Logic project not being in repo; Fix solution; Add information to represent the count of iteration and the count of living cells, display under game field

---
+++ GameLogic/Engine.cs
@@ -0,0 +1,81 @@
+﻿namespace GameOfLife
+{
+    public class Engine : IEngine
+    {
+        private readonly IGame game;
+        private int iterationCount;
+        private int livingCellCount;
+
+        /// <summary>
+        /// Initializes a new instance of the Engine class with the specified game.
+        /// </summary>
+        /// <param name="game">The game instance to manage.</param>
+        public Engine(IGame game)
+        {
+            this.game = game;
+            this.iterationCount = 0;
+            this.livingCellCount = 0;
+        }
+
+        /// <summary>
+        /// Starts the game loop with the specified update speed.
+        /// </summary>
+        /// <param name="updateSpeed">The speed at which the game updates, in milliseconds.</param>
+        public void Start(int updateSpeed)
+        {
+            while (true)
+            {
+                Console.Clear();
+                UpdateCounts();
+                PrintField();
+                game.UpdateField();
+                Thread.Sleep(updateSpeed);
+            }
+        }
+
+        /// <summary>
+        /// Prints the current state of the game field to the console.
+        /// </summary>
+        private void PrintField()
+        {
+            bool[,] field = game.Field;
+            int size = game.Size;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(field[i, j] ? Constants.LivingCell : Constants.DeadCell);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Iteration: {iterationCount}");
+            Console.WriteLine($"Living cells: {livingCellCount}");
+        }
+
+        /// <summary>
+        /// Updates the iteration count and living cell count.
+        /// </summary>
+        private void UpdateCounts()
+        {
+            iterationCount++;
+
+            livingCellCount = 0;
+
+            bool[,] field = game.Field;
+            int size = game.Size;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (field[i, j])
+                    {
+                        livingCellCount++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GameLogic/Game.cs
@@ -0,0 +1,107 @@
+﻿namespace GameOfLife
+{
+    public class Game : IGame
+    {
+        private bool[,] field;
+        private int size;
+
+        /// <summary>
+        /// Initializes a new instance of the Game class with the specified field size.
+        /// </summary>
+        /// <param name="size">The size of the field (NxN).</param>
+        public Game(int size)
+        {
+            this.size = size;
+            field = InitializeField(size);
+        }
+
+        /// <summary>
+        /// Gets the current state of the game field.
+        /// </summary>
+        public bool[,] Field => field;
+
+        /// <summary>
+        /// Gets the size of the game field.
+        /// </summary>
+        public int Size => size;
+
+        /// <summary>
+        /// Initializes the game field with random live and dead cells.
+        /// </summary>
+        /// <param name="size">The size of the field (NxN), inputted by user.</param>
+        /// <returns>A 2D boolean array representing the game field.</returns>
+        private bool[,] InitializeField(int size)
+        {
+            bool[,] field = new bool[size, size];
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    field[i, j] = random.Next(2) == 0;
+                }
+            }
+            return field;
+        }
+
+        /// <summary>
+        /// Updates the game field based on the rules of the Game of Life.
+        /// </summary>
+        /// <returns>A new 2D boolean array representing the updated game field.</returns>
+        public bool[,] UpdateField()
+        {
+            bool[,] newField = new bool[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    int liveNeighbors = CountLiveNeighbors(i, j);
+                    if (field[i, j])
+                    {
+                        newField[i, j] = liveNeighbors == 2 || liveNeighbors == 3;
+                    }
+                    else
+                    {
+                        newField[i, j] = liveNeighbors == 3;
+                    }
+                }
+            }
+
+            field = newField;
+            return field;
+        }
+
+        /// <summary>
+        /// Counts the number of live neighbors for a given cell.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the cell.</param>
+        /// <param name="y">The y-coordinate of the cell.</param>
+        /// <returns>The number of live neighbors.</returns>
+        private int CountLiveNeighbors(int x, int y)
+        {
+            int liveNeighbors = 0;
+
+            // iterating over a 3x3 grid centered at the cell specified in the arguments
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    // skips the specified cell
+                    if (i == 0 && j == 0) continue;
+
+                    int nx = x + i;
+                    int ny = y + j;
+
+                    // checks if the neighbor is within the bounds of the field and is alive
+                    if (nx >= 0 && nx < size && ny >= 0 && ny < size && field[nx, ny])
+                    {
+                        liveNeighbors++;
+                    }
+                }
+            }
+
+            return liveNeighbors;
+        }
+    }
+}
